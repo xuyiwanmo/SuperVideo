63b375c3ddd54f4fd9e49394b056d3cb598712f1
zhanghua <582605786@qq.com>
2017-09-01 17:01:27 +0800

    init

--- app/src/main/java/com/zhang/supervideo/MainActivity.java
+++ app/src/main/java/com/zhang/supervideo/MainActivity.java
@@ -3,6 +3,9 @@ package com.zhang.supervideo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * main
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
